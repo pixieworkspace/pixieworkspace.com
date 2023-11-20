a844e28ee0ac5623504fb01aa97afb1ea5d8db3f
Kennith Leung <kennith.leung@gmail.com>
2023-11-20 08:05:17 -0800

    build: use ssr to deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/scss/app.scss'],
   devtools: { enabled: true },
-  ssr: false,
+  ssr: true,
   app: {
     // baseURL: '/',
     // buildAssetsDir: '/assets/'
