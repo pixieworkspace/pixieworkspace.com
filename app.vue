fae89de5fc1e58a4029fb221da712137a6e09501
Kennith Leung <kennith.leung@gmail.com>
2023-11-21 18:34:37 -0800

    refactor: move hero to own component

--- app.vue
+++ app.vue
@@ -1,34 +1,6 @@
 <template>
-  <div class="h-screen px bg-slate-200">
-    <div class="h-full flex flex-col justify-center hero text-white
-    bg-cover
-    md:items-stretch
-    lg:h-2/3 lg:flex-row  lg:text-black lg:container lg:mx-auto
-    ">
-
-      <div class="text-center px-3
-       lg:basis-2/5 lg:text-left lg:flex lg:flex-col lg:justify-center
-       ">
-        <div class="text-5xl font-extrabold my-10">
-          Technology is vital to business growth
-        </div>
-        <div class="my-5">We take care of the tech, so you can focus on yours.</div>
-        <div class="max-w-xs mx-auto lg:max-w-fit lg:mx-0 px-0">
-          <div class="text-center uppercase mt-5 p-5 bg-slate-600 rounded-md text-white
-          lg:text-left
-          ">
-            <a href="mailto:hello@pixieworkspace.com">Contact us now</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="hidden lg:block lg:grow lg:bg-slate-100 bg-cover hero-lg">
-      </div>
-    </div>
-
-  </div>
+  <Hero />
 </template>
-
 <script setup lang="ts">
 useHead({
   title: 'Pixie - Web Application Development Partner',
